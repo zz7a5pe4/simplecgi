f6d0dbc0d6ff80aa729c97b7e8d9bbc9b45b8192
ryan <ryan20101108@gmail.com>
2012-09-13 13:06:23 +0800

    fixed the scroll and define a default the configuration in index.html

--- cmdmapping.py
+++ cmdmapping.py
@@ -3,6 +3,6 @@
 cmdmaps={"ls":"ls -l /home",
         "pwd":"pwd",
         "who":"w",
-        "ping":"ping 127.0.0.1 -c 10",
+        "ping":"ping 127.0.0.1 -c 30",
         "update":"apt-get update",
         "x7":"/home/xeven/deploy.sh"}
--- static/js/submit.js
+++ static/js/submit.js
@@ -1,32 +1,14 @@
 $(document).ready(function(){
 	var fieldsetCount = $('#paramForm').children().length;
 	var autoload = 0;
-	var jsonObj = "";
-
+	
 	initConfigurationPage();
 	stop(); //init the iframe
 
 	$("#FLAT_INTERFACE").change(function(){
 		var flat_interface = $("#FLAT_INTERFACE").val();
 		if(flat_interface != "" || flat_interface != null){
-			
-			jQuery.ajax({
-				type: "get",
-				url: "http://127.0.0.1:8000/ip_query?i="+flat_interface,
-				dataType: "text",
-				
-				success: function(data, dataStatus){
-					var obj = JSON.parse(data);
-					$("#FLOATING_RANGE").val(obj.brdcast+"\/25");
-				},
-				complete: function(XMLHttpRequest, textStatus){
-					//do something
-				},
-				error: function(){
-					alert("Connection Error!");
-		   			return;
-				}
-			});
+			getNetworkConfiguration( flat_interface );
 		}
 	});
 	
@@ -41,9 +23,8 @@ $(document).ready(function(){
         }
 		else {
 			$("#wrapper").hide();
-			$("iframe").show();
+			$("#resultframe").show();
 			postTheX7Configuration();
-			refresh();
 		}
 	});
 	
@@ -73,39 +54,22 @@ $(document).ready(function(){
 			volumn_selected_result = $("#DEVICE_PATH").val();
 		}
 		
-		jsonObj = {"Select_x7":x7selected, "FLAT_INTERFACE":flat_interface, "FIXED_RANGE":fixed_range, "FIXED_NETWORK_SIZE":fixed_network_size,
+		var jsonObj = {"Select_x7":x7selected, "FLAT_INTERFACE":flat_interface, "FIXED_RANGE":fixed_range, "FIXED_NETWORK_SIZE":fixed_network_size,
 				"MYSQL_PASSWORD":mysql_password,"RABBIT_PASSWORD":rabbit_password,"SERVICE_PASSWORD":service_password,"ADMIN_PASSWORD":admin_password,
 				"SSD_PATH":ssd_path, "VOLUMN_SELECTED":volumn_selected, "VOLUMN_SELECTED_RESULT":volumn_selected_result, "FLOATING_RANGE":floating_range};
 		
-		// jQuery.ajax({
-		// 	type: "post",
-		// 	url: "http://127.0.0.1:8000/x7",
-		// 	dataType: "json",
-		// 	data: jsonObj,
-			
-		// 	success: function(data, dataStatus){
-		// 		//something
-		// 		//refresh();
-		// 	},
-		// 	complete: function(XMLHttpRequest, textStatus){
-		// 		//HideLoading();
-	 //   		},
-	 //   		error: function(){
-	 //   			//alert("Connection Error!");
-	 //   			//return;
-	 //   		}
-		// });
+		refresh(jsonObj);
 	}
-	
+
 	/*
 	 * get x7 install callback information
 	 */
-	function refresh() {
+	function refresh(json) {
 		document.getElementById('submitBtn').style.visibility='hidden';
-		post_to_url("http://127.0.0.1:8000/x7", jsonObj);
+		post_to_url("http://127.0.0.1:8000/x7", json);
 		autoload = window.setInterval('autoscroll()', 100);
 	}
-	
+
 	function post_to_url(path, params, method) {
 		method = method || "post"; // Set method to post by default, if not specified.
 		
@@ -128,12 +92,7 @@ $(document).ready(function(){
 		document.body.appendChild(form);
 		form.submit();
 	}
-	
-	function autoscroll() {
-		var pos_y =  document.getElementById("resultframe").contentWindow.document.body.scrollHeight + 10;
-		document.getElementById("resultframe").contentWindow.scrollTo(0, pos_y );
-	}
-	
+
 	function stop(){
 		autoscroll();
 		window.clearInterval(autoload);
@@ -141,16 +100,30 @@ $(document).ready(function(){
 	}
 	
 	/*
-	 * init the configuration for the radios.
+	 * init the configuration of the setup pages.
 	 */
 	function initConfigurationPage(){
-		var volumnPara = null;
+		var $x7this = $("[name='Select_x7']:checked");
+		$x7this.nextAll("textarea").show();
+		$x7this.nextAll("textarea").prev(".textarea_name").show();
+		
+		var $volumnthis = $(":radio[name='VOLUMN_SELECTED']:checked");
+		var volumnPara = $volumnthis.val();
+		if(volumnPara == "File"){
+			$volumnthis.nextAll(".hidden-1").css("display","inline-block").show();
+		}
+		else if(volumnPara == "Device"){
+			$volumnthis.nextAll(".hidden-2").css("display","inline-block").show();
+		}
+		
+		var defaultFlatInterface = $("#FLAT_INTERFACE").val();	
+		getNetworkConfiguration( defaultFlatInterface );
 		
 		$("[name='Select_x7']").click(function(){
 			$("textarea").hide();
 			$(".textarea_name").hide();
 			
-			if($("[name='Select_x7']:checked")[0].checked){
+			if($(this)[0].checked){
 				$(this).nextAll("textarea").show();
 				$(this).nextAll("textarea").prev(".textarea_name").show();
 			}
@@ -159,8 +132,6 @@ $(document).ready(function(){
 		$(":radio[name='VOLUMN_SELECTED']").click(function(){
 			$(".hidden-1").hide();
 			$(".hidden-2").hide();
-			$("input.hidden-1").val("");
-			$("input.hidden-2").val("");
 			
 			if($(":radio[name='VOLUMN_SELECTED']:checked")[0].checked){
 				volumnPara = $(this).val();
@@ -170,6 +141,34 @@ $(document).ready(function(){
 				else if(volumnPara == "Device"){
 					$(this).nextAll(".hidden-2").css("display","inline-block").show();
 				}
+			}
+		});
+	}
+	
+	/*
+	 * get the network configuration by FLAT INTERFACE
+	 */
+	function getNetworkConfiguration( flat_interface ){
+		jQuery.ajax({
+			type: "get",
+			url: "http://127.0.0.1:8000/ip_query?i="+flat_interface,
+			dataType: "text",
+			
+			success: function(data, dataStatus){
+				var obj = JSON.parse(data);
+				if(obj.brdcast == "" || obj.brdcast == null){
+					alert("Please fill the correct flat interface in Network, such as eth0 eth1 wlan0 etc.");
+				}
+				else{
+					$("#FLOATING_RANGE").val(obj.brdcast+"\/25");
+				}
+			},
+			complete: function(XMLHttpRequest, textStatus){
+				//do something
+			},
+			error: function(){
+				alert("Connection Error!");
+	   			return;
 			}
 		});
 	}
@@ -232,3 +231,8 @@ $(document).ready(function(){
     }
 	
 });
+
+function autoscroll() {
+	var pos_y =  document.getElementById("resultframe").contentWindow.document.body.scrollHeight + 10;
+	document.getElementById("resultframe").contentWindow.scrollTo(0, pos_y );
+}
